b40cb412e0b8be74eae580863a87a8ed8e7274ec
Alex Key <alex.key@kawunu.com>
2025-07-23 13:49:47 +0200

    remove outdated code

--- dotenv_loader.py
+++ dotenv_loader.py
@@ -1,8 +1,6 @@
 from dotenv import load_dotenv
 import os
-from pathlib import Path
 
-# Get the current working directory and go up one level
 load_dotenv(override=True)
 api_key = os.getenv("OPENAI_API_KEY")
 tavily_api_key = os.getenv("TAVILY_API_KEY")
