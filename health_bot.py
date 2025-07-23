2552929e9eceee48ea2b2db2e513e19faf564c5a
Alex Key <alex.key@kawunu.com>
2025-07-23 13:11:50 +0200

    remove old code

--- health_bot.py
+++ health_bot.py
@@ -316,49 +316,6 @@ def hitl_health_bot(graph: CompiledStateGraph):
             print("Got it, goodbye!")
             break
 
-            ### old code
-            # if quiz_requested.lower() in ["yes", "y"]:
-            #
-            #     # Quiz was requested, generate a question
-            #     for event in graph.stream(input=None, config=config,
-            #                               stream_mode="values"):
-            #         if event.get("messages"):
-            #             message = event["messages"][-1]
-            #             if message.id != last_printed_message_id:
-            #                 message.pretty_print()
-            #                 last_printed_message_id = message.id
-            #
-            #     # Get the user's answer and make sure it's long enough
-            #     quiz_answer = input("Please state your answer\n"
-            #                         "> ")
-            #     while not quiz_answer.strip() or len(quiz_answer) < 5:
-            #         quiz_answer = input(
-            #             "It looks like you haven't entered an answer.
-            #             Please "
-            #             "try again.\n"
-            #             "> ")
-            #
-            #     # Continue with grading the answer
-            #     graph.update_state(config, {"quiz_answer": quiz_answer})
-            #
-            #     for event in graph.stream(input=None,
-            #                               config=config,
-            #                               stream_mode="values"):
-            #         if event.get("messages"):
-            #             message = event["messages"][-1]
-            #             if message.id != last_printed_message_id:
-            #                 message.pretty_print()
-            #                 last_printed_message_id = message.id
-            #
-            # # Restart with another topic or exit
-            # user_proceeds = input(
-            #     "Would you like to talk about another topic ("
-            #     "y/n)?\n"
-            #     "> ")
-            # if user_proceeds.lower() not in ["yes", "y"]:
-            #     print("\nExiting.")
-            #     break
-
 
 if __name__ == "__main__":
     hitl_health_bot(graph=graph)
